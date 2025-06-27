fd201e2fe64bab3b15e13a2eab6d62ea8cfa1af0
Kachalaba Mykyta <88351178+Kachalaba@users.noreply.github.com>
2025-06-28 00:52:34 +0300

    keyboards: translate button texts

--- keyboards.py
+++ keyboards.py
@@ -13,10 +13,10 @@ def get_stroke_keyboard() -> InlineKeyboardMarkup:
     """Return keyboard for stroke selection."""
     builder = InlineKeyboardBuilder()
     builder.button(
-        text="Вольный стиль", callback_data=StrokeCB(stroke="freestyle").pack()
+        text="Вільний стиль", callback_data=StrokeCB(stroke="freestyle").pack()
     )
-    builder.button(text="Баттерфляй", callback_data=StrokeCB(stroke="butterfly").pack())
-    builder.button(text="Брасс", callback_data=StrokeCB(stroke="breaststroke").pack())
-    builder.button(text="На спине", callback_data=StrokeCB(stroke="backstroke").pack())
+    builder.button(text="Батерфляй", callback_data=StrokeCB(stroke="butterfly").pack())
+    builder.button(text="Брас", callback_data=StrokeCB(stroke="breaststroke").pack())
+    builder.button(text="На спині", callback_data=StrokeCB(stroke="backstroke").pack())
     builder.adjust(2)
     return builder.as_markup()
